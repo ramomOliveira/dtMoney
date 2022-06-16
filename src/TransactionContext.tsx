748220cac37bef6d698f5868b3f9773c4f2b6a1e
Ramom Oliveira <ra-mom2000@hotmail.com>
2022-06-16 17:56:57 -0300

    calculation of deposits made

--- src/TransactionContext.tsx
+++ src/TransactionContext.tsx
@@ -27,7 +27,7 @@ interface TransactionProviderProps {
 
 interface TransactionsContextDataProps {
   transactions: TransactionProps[];
-  createTransaction: (transaction: TransactionInputsProps) => void;
+  createTransaction: (transaction: TransactionInputsProps) => Promise<void>;
 }
 
 export const TransactionContext = createContext<TransactionsContextDataProps>({} as TransactionsContextDataProps);
@@ -41,8 +41,14 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
     api.get('transactions').then(response => setTransactions(response.data.transactions))
   }, []);
 
-  function createTransaction(transaction: TransactionInputsProps) {
-    api.post('/transactions', transaction);
+  async function createTransaction(transactionInput: TransactionInputsProps) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+    const { transaction } = response.data;
+
+    setTransactions([...transactions, transaction]);
   }
 
   return (
